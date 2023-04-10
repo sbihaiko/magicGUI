c42a22752932457060282ca99fb2f3a6aff83174
sbihaiko <122891372+sbihaiko@users.noreply.github.com>
2023-04-10 19:56:19 -0300

    Update animations.js

--- animations.js
+++ animations.js
@@ -1,55 +1,14 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Meu motor de animação</title>
-    <style>
-      #container {
-        width: 600px;
-        height: 200px;
-        position: relative;
-      }
-      #seconds {
-        position: absolute;
-        bottom: 10px;
-        left: 10px;
-        padding: 10px;
-        font-size: 24px;
-        font-weight: bold;
-        color: white;
-      }
-    </style>
-  </head>
-  <body>
-    <div id="container">
-      <div id="box"></div>
-      <div id="seconds"></div>
-    </div>
+// Variável que impede a execução duplicada do código
+var version = '1.0.0';
 
-    // mostra os segundos
-    <script>
-      setInterval(function() {
-        var now = new Date();
-        var seconds = now.getSeconds();
-        var secondsElement = document.getElementById('seconds');
-        secondsElement.innerHTML = 'Segundos: ' + seconds;
-      }, 1000);
-    </script>
+// Função para animar a transição de duas cores
+function animateColors() {
+  var box = document.getElementById('box');
+  box.style.backgroundColor = 'orange';
+}
 
-    <script>
-      var currentVersion = '1.0.0';
-      var previousVersion = '';
-
-      // Loop que carrega o arquivo animation.js se houver uma nova versão
-      setInterval(function() {
-        if (previousVersion !== currentVersion) {
-          var script = document.createElement('script');
-          script.src = 'animation.js';
-          document.head.appendChild(script);
-          previousVersion = currentVersion;
-        }
-      }, 100);
-    </script>
-
-  </body>
-</html>
+// Verifica se a versão atual já foi carregada
+if (typeof animationVersion == 'undefined' || animationVersion != version) {
+  animationVersion = version;
+  animateColors();
+}
