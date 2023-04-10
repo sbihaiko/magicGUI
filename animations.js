26a1b504c0d83b02294e54fa306e8729d3ed9904
sbihaiko <122891372+sbihaiko@users.noreply.github.com>
2023-04-10 19:59:43 -0300

    Update animations.js

--- animations.js
+++ animations.js
@@ -1,5 +1,5 @@
-// Variável que impede a execução duplicada do código
-var version = '1.0.0';
+// Armazena a versão atual
+var version = '1.0.1';
 
 // Função para animar a transição de duas cores
 function animateColors() {
@@ -11,4 +11,21 @@ function animateColors() {
 if (typeof animationVersion == 'undefined' || animationVersion != version) {
   animationVersion = version;
   animateColors();
+
+  // Adiciona o contador de segundos
+  var secondsElement = document.createElement('div');
+  secondsElement.id = 'seconds';
+  document.getElementById('container').appendChild(secondsElement);
+
+  setInterval(function() {
+    var now = new Date();
+    var seconds = now.getSeconds();
+    secondsElement.innerHTML = 'Segundos: ' + seconds;
+  }, 1000);
+
+  // Remove o contador de segundos do arquivo index.html
+  var secondsElementIndex = document.getElementById('seconds');
+  if (secondsElementIndex) {
+    secondsElementIndex.parentNode.removeChild(secondsElementIndex);
+  }
 }
